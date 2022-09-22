17fb41f77d7791a84fa2a144d64f0a46924f9faa
Marcos Chaves Paim <marcos.paim@trensurb.com.br>
2022-09-22 09:47:51 -0300

    change index.py

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 import requests
 from requests.auth import HTTPBasicAuth
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
+gps_dict = {}
+last_request = datetime.now()
+
 
 @app.route("/")
 def hello_world():
@@ -10,14 +15,19 @@ def hello_world():
 
 @app.route("/gps")
 def gps():
+    global last_request
+    global gps_dict
+    now = datetime.now()
     response = None
-    #try:
-    response = requests.get("http://gps-trens.trensurb.gov.br:5244/coordenadas/api/v1.0/GPS",auth=HTTPBasicAuth('susan', 'bye'))
-    response = response.json()
-    #except:
-        #print("Nao foi possivel conectar-se com a api")
+    if(now-last_request > timedelta(seconds=3.9)):
+        try:
+            response = requests.get("http://gps-trens.trensurb.gov.br:5244/coordenadas/api/v1.0/GPS",auth=HTTPBasicAuth('susan', 'bye'))
+            gps_dict = response.json()
+            last_request = datetime.now()
+        except:
+            print("Nao foi possivel conectar-se com a api")
     
-    return response, 200
+    return gps_dict, 200
 
 #if __name__ == '__main__':
 #    app.run()
